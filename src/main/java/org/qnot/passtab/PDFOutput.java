b466c3f761a8e3f2b34bb2f23c4351a9882661c0
Andrew E. Bruno <aeb@qnot.org>
2011-06-25 00:39:51 -0400

    Switched to iText for PDF generation. Because of this also had to change to GPLv3 license. Added dejavu fonts for Unicode support.

--- src/main/java/org/qnot/passtab/PDFOutput.java
+++ src/main/java/org/qnot/passtab/PDFOutput.java
@@ -1,109 +1,133 @@
 /*
- * Copyright 2011 Andrew E. Bruno <aeb@qnot.org>
+ * Copyright (C) 2011  Andrew E. Bruno <aeb@qnot.org>
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy
- * of the License at 
+ * This file is part of passtab.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0 
+ * passtab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
+ * passtab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with passtab.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.qnot.passtab;
 
-import java.awt.Color;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDFont;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
-/**
- * Output Tabula Recta in PDF format
- * 
- * @author Andrew E. Bruno <aeb@qnot.org>
- */
 public class PDFOutput implements OutputFormat {
-    private static int X_START = 140;
-    private static int Y_START = 570;
+    private static String FONT_MONOSPACE = "org/qnot/passtab/fonts/DejaVuSansMono.ttf";
+    private static String FONT_MONOSPACE_BOLD = "org/qnot/passtab/fonts/DejaVuSansMono-Bold.ttf";
+    private static int CELL_WIDTH = 15;
+    private static float DEFAULT_FONT_SIZE = 12f;
 
-    private static int X_SPACE = 15;
-    private static int Y_SPACE = 15;
-
-    private static int BOX_WIDTH = 14;
-    private static Color BOX_COLOR = new Color(204, 204, 204);
-    private static Color FONT_COLOR = Color.BLACK;
-
-    private static PDFont DEFAULT_FONT = PDType1Font.COURIER;
-    private static PDFont BOLD_FONT = PDType1Font.COURIER_BOLD;
-    
     private boolean withColor;
-    
-    public PDFOutput() { 
-        this.withColor = true;
+    private Font font;
+    private Font fontBold;
+
+    public PDFOutput() {
+        this(true);
     }
-    
+
     public PDFOutput(boolean withColor) {
         this.withColor = withColor;
-    }
-    
-    public void output(OutputStream out, TabulaRecta tabulaRecta) throws IOException {
-        PDDocument document = new PDDocument();
-        PDPage page = new PDPage();
-        page.setMediaBox(PDPage.PAGE_SIZE_A4);
-        page.setRotation(90);
-        document.addPage( page );
-        float pageWidth = PDPage.PAGE_SIZE_A4.getWidth();
-
-        PDPageContentStream contentStream = new PDPageContentStream(document, page);
-        contentStream.concatenate2CTM(0, 1, -1, 0, pageWidth, 0);
-        contentStream.setFont(PDFOutput.DEFAULT_FONT, 12);
-
-        int x = PDFOutput.X_START;
-        int y = PDFOutput.Y_START;
-
-        String[][] array = tabulaRecta.asStringArray();
-        
-        for(int i = 0; i < array.length; i++) {
-            for(int j = 0; j < array[0].length; j++) {
-                if(this.withColor && (i % 2) != 0) {
-                    contentStream.setNonStrokingColor(PDFOutput.BOX_COLOR);
-                    contentStream.fillRect(x-2, y-4, PDFOutput.BOX_WIDTH, PDFOutput.BOX_WIDTH);
-                    contentStream.setNonStrokingColor(PDFOutput.FONT_COLOR);
-                }
-                if(this.withColor && (j % 2) != 0) {
-                    contentStream.setFont(PDFOutput.BOLD_FONT, 12);
-                } else {
-                    contentStream.setFont(PDFOutput.DEFAULT_FONT, 12);
-                }
-
-                contentStream.beginText();
-                contentStream.moveTextPositionByAmount(x,y);
-                contentStream.drawString(array[i][j]);
-                contentStream.endText();
-                x += PDFOutput.X_SPACE;
-            }
-            y -= PDFOutput.Y_SPACE;
-            x = PDFOutput.X_START;
-        }
-
-        contentStream.drawLine(130, 567, 695, 567);
-        contentStream.drawLine(152, 585, 152, 20);
-        contentStream.close();
 
         try {
-            document.save(out);
-            document.close();
-        } catch(Exception e) {
-            throw new IOException("Failed to create PDF: ", e);
+            font = new Font(BaseFont.createFont(PDFOutput.FONT_MONOSPACE,
+                    BaseFont.IDENTITY_H, BaseFont.EMBEDDED));
+            fontBold = new Font(BaseFont.createFont(
+                    PDFOutput.FONT_MONOSPACE_BOLD, BaseFont.IDENTITY_H,
+                    BaseFont.EMBEDDED));
+        } catch (Exception e) {
+            try {
+                font = new Font(BaseFont.createFont());
+                fontBold = new Font(BaseFont.createFont());
+            } catch (Exception ignored) {
+            }
         }
+
+        font.setSize(DEFAULT_FONT_SIZE);
+        fontBold.setSize(DEFAULT_FONT_SIZE);
+    }
+
+    public void output(OutputStream out, TabulaRecta tabulaRecta) throws IOException {
+        try {
+            createPDF(out, tabulaRecta.asStringArray());
+        } catch (DocumentException e) {
+            throw new IOException("failed to create pdf file: "
+                    + e.getMessage(), e);
+        }
+    }
+
+    private void createPDF(OutputStream out, String[][] array)
+            throws IOException, DocumentException {
+        Document document = new Document(PageSize.LETTER.rotate());
+        PdfWriter.getInstance(document, out);
+        document.open();
+
+        PdfPTable table = new PdfPTable(array[0].length);
+        table.setTotalWidth((float) array.length * PDFOutput.CELL_WIDTH);
+        table.setLockedWidth(true);
+        table.setHorizontalAlignment(Element.ALIGN_CENTER);
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                addCell(table, array[i][j], (j % 2) != 0, (i % 2) != 0, j == 0,
+                        i == 0);
+            }
+        }
+
+        document.add(table);
+        document.close();
+    }
+
+    private void addCell(PdfPTable table, String str, boolean bold,
+            boolean fill, boolean rightBorder, boolean bottomBorder) {
+        Phrase phrase = withColor ? new Phrase(str, bold ? fontBold : font)
+                : new Phrase(str, font);
+
+        PdfPCell cell = new PdfPCell(phrase);
+
+        cell.setBorder(Rectangle.NO_BORDER);
+        cell.setPadding(0f);
+        cell.setPaddingTop(2f);
+
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+        if (rightBorder) {
+            cell.setBorderWidthRight(1f);
+            cell.setPaddingRight(3f);
+            cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+        }
+        if (bottomBorder) {
+            cell.setBorderWidthBottom(1f);
+            cell.setPaddingBottom(3f);
+        }
+
+        if (fill && this.withColor) {
+            cell.setGrayFill(0.80f);
+        }
+
+        table.addCell(cell);
     }
 }
